e739bf94c6afa826e1b8e405e479ba7a77d3498f
alfredo <alfredo.puentes25@outlook.com>
2025-08-26 13:34:41 -0600

    se agrego comentario en el main

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //Alfredo Puentes Vargas
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
